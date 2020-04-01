83aa1d92c986d5c337beddc95b71b2f4268ecff4
liujun <liujuna@uniontech.com>
2020-04-01 14:12:27 +0800

    rewrite audiodevicewidget

--- src/audiodevicewidget.cpp
+++ src/audiodevicewidget.cpp
@@ -30,7 +30,160 @@ DWIDGET_USE_NAMESPACE
 
 AudiodeviceWidget::AudiodeviceWidget(QWidget *parent) : DeviceInfoWidgetBase(parent, tr("Sound Adapter"))
 {
-    initWidget();
+    //initWidget();
+
+    // 先从lshw命令和cat /proc/bus/input/devices 中获取音频列表
+    QStringList multimediaList,inputdeviceList;
+    int totalSize = getAudioList(multimediaList,inputdeviceList);
+
+    // 如果都没有则直接返回
+    if( totalSize < 1 ){
+        setCentralInfo(tr("No sound adapter found"));
+        return;
+    }
+
+    // ***************************************************************************************
+    QList<QStringList> tabList;
+    // 从lshw中获取显示信息
+    getInfoFromLshw(tabList,multimediaList);
+
+    // 从cat /proc/bus/input/devices中获取信息
+    getInfoFromCatInput(tabList,inputdeviceList);
+
+    // 添加表格内容
+    QStringList headers = { tr("Name"),  tr("Vendor")};
+    addTable( headers, tabList);
+
+}
+
+int AudiodeviceWidget::getAudioList(QStringList& lshwList,QStringList& catList)
+{
+    lshwList = DeviceInfoParser::Instance().getLshwMultimediaList();
+    catList  = DeviceInfoParser::Instance().getCatinputAudioDeviceList();
+    return lshwList.size() + catList.size();
+}
+
+void AudiodeviceWidget::getInfoFromLshw(QList<QStringList>& tabList,const QStringList& lshwList)
+{
+    foreach(auto lshwMedia, lshwList){
+        QStringList tabItem;
+        addLshwAudioInfo(lshwMedia,tabItem);
+        tabList.append(tabItem);
+    }
+}
+
+void AudiodeviceWidget::addLshwAudioInfo(const QString& item,QStringList& tabItem)
+{
+    QList<ArticleStruct> articles;
+    QSet<QString> existArticles;
+
+    // 获取总线信息 和 名称
+    ArticleStruct busInfo(tr("Bus Info","Audio Device Info"));
+    ArticleStruct name(tr("Name","Audio Device Info"));
+    getBusInfoAndName(item,busInfo,name,existArticles);
+
+    // 获取设备商信息
+    ArticleStruct vendor(tr("Vendor","Audio Device Info"));
+    vendor.queryData( "lshw", item, "vendor", existArticles);
+
+    // 获取产品描述信息
+    ArticleStruct description(tr("Description"));
+    description.queryData("lshw", item, "description", existArticles);
+
+    // 这一句没有琢磨是什么意思
+    if(name.value == vendor.value){
+        name.value = description.value;
+    }
+
+    // *******************************************************
+    articles.push_back(name);
+    articles.push_back(vendor);
+    articles.push_back(description);
+    articles.push_back(busInfo);
+
+    // 添加设备的其它信息
+    DeviceInfoParser::Instance().queryRemainderDeviceInfo("lshw", item, articles, existArticles,
+                                                          "ManulTrack__AudioDevices","Audio device infomation from lshw");
+    //addDevice( name.value , articles,1);
+    addSubInfo( name.value , articles );
+
+    // 获取表格内容
+    tabItem.append(name.value.remove(vendor.value));
+    tabItem.append(vendor.value);
+
+    // 获取概况
+    if( overviewInfo_.value.isEmpty() == false ){
+        overviewInfo_.value += " / ";
+    }
+    if(vendor.isValid() && name.value.contains(vendor.value, Qt::CaseInsensitive)==false){
+        overviewInfo_.value += vendor.value;
+        overviewInfo_.value += " ";
+    }
+    overviewInfo_.value += name.value;
+}
+
+void AudiodeviceWidget::addCatAudioInfo(const QString& item,QStringList& tabItem)
+{
+    QList<ArticleStruct> articles;
+    QSet<QString> existArticles;
+
+    // 获取名称
+    ArticleStruct name(tr("Name"));
+    name.queryData( "catinput", item, "Name");
+    name.value.remove("\"");
+    articles.push_back(name);
+    existArticles.insert("Name");
+
+    // 获取设备商
+    ArticleStruct vendor(tr("Vendor"));
+    vendor.queryData( "catinput", item, "Vendor" , existArticles, articles);
+
+    // 获取版本信息
+    ArticleStruct vesion(tr("Version"));
+    vesion.queryData( "catinput", item, "Version", existArticles, articles);
+
+    // 获取Sysfs信息
+    ArticleStruct sysfs(tr("Sysfs","Audio Device Info"));
+    sysfs.queryData( "catinput", item, "Sysfs", existArticles, articles);
+
+    // 添加其他的设备信息
+    DeviceInfoParser::Instance().queryRemainderDeviceInfo("catinput", item, articles, existArticles,
+                                                          "ManulTrack__AudioDevices","Audio device infomation from catinput");
+    addSubInfo( name.value , articles );
+
+    // 获取表格内容
+    tabItem.append(name.value);
+    tabItem.append(vendor.value);
+}
+
+void AudiodeviceWidget::getBusInfoAndName(const QString& item,ArticleStruct& busInfo,ArticleStruct& name,QSet<QString>& existArticles)
+{
+    busInfo.queryData("lshw", item, "bus info", existArticles);
+    QRegExp reg("^pci@[0-9]*:([\\s\\S]*)$");
+    QString pci_bus;
+    if(reg.exactMatch(busInfo.value)){
+        pci_bus= reg.cap(1);
+    }
+
+    QString lspciDeviceName;
+    DeviceInfoParser::Instance().fuzzeyQueryKey("lspci", pci_bus, lspciDeviceName);
+
+    name.queryData("lspci", lspciDeviceName, "Name");
+    name.queryData( "lshw", item, "product", existArticles);
+    name.value.remove( " Corporation", Qt::CaseInsensitive );
+    int index = name.value.indexOf('(');
+    if(index > 0){
+        name.value = name.value.mid(0, index);
+    }
+}
+
+void AudiodeviceWidget::getInfoFromCatInput(QList<QStringList>& tabList,const QStringList& catList)
+{
+    foreach(auto catMedia, catList){
+        QStringList tabItem;
+        addCatAudioInfo(catMedia,tabItem);
+        tabList.append(tabItem);
+    }
 }
 
 void AudiodeviceWidget::initWidget()
--- src/audiodevicewidget.h
+++ src/audiodevicewidget.h
@@ -30,5 +30,28 @@ class AudiodeviceWidget : public DeviceInfoWidgetBase
 public:
     explicit AudiodeviceWidget(QWidget *parent = nullptr);
     void initWidget() override;
+
+    /**
+     * @brief:获取音频设备列表
+     * @param[in/out]:lshwList --> 从lshw中获取的音频信息
+     * @param[in/out]:catList  --> 从cat /proc/bus/input/devices 中获取音频信息
+     * @return[int]:返回一共有多少个音频，lshwList + catList
+     */
+    int getAudioList(QStringList& lshwList,QStringList& catList);
+    void addLshwAudioInfo(const QString& item,QStringList& tabItem);
+    void addCatAudioInfo(const QString& item,QStringList& tabItem);
+    void getBusInfoAndName(const QString& item,ArticleStruct& busInfo,ArticleStruct& name,QSet<QString>& existArticles);
+
+    /**
+     * @brief:从lshw中获取音频设备信息
+     * @param[in/out]:tabList --> 用于记录存储在表格里面的内容
+     */
+    void getInfoFromLshw(QList<QStringList>& tabList,const QStringList& lshwList);
+
+    /**
+     * @brief:从cat /proc/bus/input/devices中获取音频设备信息
+     * @param[in/out]:tabList --> 用于记录存储在表格里面的内容
+     */
+    void getInfoFromCatInput(QList<QStringList>& tabList,const QStringList& catList);
 };
 
