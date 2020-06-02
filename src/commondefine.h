6aff26f61b4192a881c0193816f7b245b2b62c25
jixiaomei <jixiaomei@uniontech.com>
2020-06-02 13:35:25 +0800

    fix:注释commondefine.h中的调试宏定义
    Description: 注释commondefine.h中的调试宏定义
    
    Log: 注释commondefine.h中的调试宏定义
    
    link:

--- src/commondefine.h
+++ src/commondefine.h
@@ -52,6 +52,5 @@ const int spinnerHeight = 32;
 
 const int TableViewRowHeight_ = 36;
 #define GenerateTsItem 0
-#define TEST_DATA_FROM_FILE
+//#define TEST_DATA_FROM_FILE
 const QString DEVICEINFO_PATH = "/home/liujun/Work/CommonlyUsed/newDeviceinfo/HuaWei/KLU";
-
