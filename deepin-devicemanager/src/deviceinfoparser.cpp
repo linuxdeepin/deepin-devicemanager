ef6f8ed3c501f3e5978c13bcb25aeece11cd4a8f
UT000650 <liujuna@uniontech.com>
2020-07-07 17:30:19 +0800

    fix: KLU调试信息
    Description: KLU调试信息
    
    Log: KLU调试信息
    
    link:

--- deepin-devicemanager/src/deviceinfoparser.cpp
+++ deepin-devicemanager/src/deviceinfoparser.cpp
@@ -87,6 +87,7 @@ QString DeviceInfoParser::loadGeneratorKey()
         return key;
     }
 
+    qDebug() << "@@@@@@@" <<deviceInfo << "@@@@@@@@";
     if (deviceInfo.contains("PGU-WBY0")) {  // pangu
         key = "PanGu";
     } else if (deviceInfo.contains("HUAWEI L410 KLVU-WDU0")) { // klu
