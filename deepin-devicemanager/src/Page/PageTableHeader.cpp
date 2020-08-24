cfde2b1f1bfdefa05bf421f95c22a93db4be7269
liujun <liujuna@uniontech.com>
2020-08-24 16:55:03 +0800

    fix: 更新翻译文件
    Description: 更新翻译文件
    
    Log: 更新翻译文件
    
    link:

--- deepin-devicemanager/src/Page/PageTableHeader.cpp
+++ deepin-devicemanager/src/Page/PageTableHeader.cpp
@@ -58,7 +58,7 @@ void PageTableHeader::updateTable(const QList<QStringList> &lst)
     int row = lst.size();
     int column = lst[0].size() - 1;
     for (int i = 0; i < row - 1; i++) {
-        bool enable = lst[i + 1][0].startsWith("(" + tr("禁用") + ")");
+        bool enable = lst[i + 1][0].startsWith("(" + tr("Disable") + ")");
         int co = column;
         if (enable) {co = 1;}
         for (int j = 0; j < co; j++) {
