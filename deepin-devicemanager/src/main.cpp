4d170b0ccae4362d3fde7dd9603bcb67fe670c55
liujun <liujuna@uniontech.com>
2020-09-02 15:01:07 +0800

    fix: 移除DApplication::loadDXcbPlugin();
    Description: 移除DApplication::loadDXcbPlugin();
    
    Log: 移除DApplication::loadDXcbPlugin();
    
    link:
    Change-Id: I29145823035e23b11649f0526473bd66fafeeb0d
    Reviewed-on: http://gerrit.uniontech.com/c/nanjing/deepin-devicemanager/+/3341
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: liujun <liujuna@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- deepin-devicemanager/src/main.cpp
+++ deepin-devicemanager/src/main.cpp
@@ -36,8 +36,6 @@ DWIDGET_USE_NAMESPACE
 int main(int argc, char *argv[])
 {
     QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-
-    DApplication::loadDXcbPlugin();
     DApplication app(argc, argv);
 
     app.loadTranslator();
