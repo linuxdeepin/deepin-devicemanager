eea74a473238fda029e873de5b53a8ffafd6f86f
liujun <liujuna@uniontech.com>
2020-10-10 18:02:08 +0800

    fix: 空提交，用于打tag号
    空提交，用于打tag号
    
    空提交，用于打tag号
    
    link:
    Change-Id: Ib83c512df7770f1634e12cd52bcd6225863e80f8
    Reviewed-on: http://gerrit.uniontech.com/c/nanjing/deepin-devicemanager/+/7055
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: liujun <liujuna@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- deepin-devicemanager/src/commondefine.h
+++ deepin-devicemanager/src/commondefine.h
@@ -53,4 +53,4 @@ const int spinnerHeight = 32;
 const int TableViewRowHeight_ = 36;
 #define GenerateTsItem 0
 //#define TEST_DATA_FROM_FILE
-//const QString DEVICEINFO_PATH = "/home/liujun/Work/Job/GetHardwareInfo/deviceInfo/Bug/Bug_37163";
+//const QString DEVICEINFO_PATH = "/home/liujun/Work/Job/GetHardwareInfo/deviceInfo/Bug/Bug_49431";
