739e77598e5e36bdf2da0785dc76018cda797af8
liujun <liujuna@uniontech.com>
2020-12-30 14:46:44 +0800

    fix: 修改配置文件
     修改QtxlsxWriter的LICESEN为MIT
     修改copyright文件
     修改desktop文件
    
    Log: 修改配置文件
    Change-Id: If920fcf7deaef8ccab855f4445cf8b58fd4e375e

--- deepin-devicemanager-server/src/main.cpp
+++ deepin-devicemanager-server/src/main.cpp
@@ -11,31 +11,6 @@
 
 int main(int argc, char *argv[])
 {
-//    int n = fork();
-//    fprintf(stdout, "PID:%d, PGID:%d, SID:%d\n", getpid(), getpgrp(), getsid(0));
-
-//    int stat = 0;
-//    if (n > 0) {
-//        pid_t x = wait(&stat);
-//        printf("child pid=%d\n", x);
-//        if (WIFEXITED(stat) == 0) {
-//            printf("child normal exited!\n");
-//        }
-//    } else if (n == 0) {
-
-//        if (setsid() < 0) {
-//            fprintf(stderr, "setsid error:%s\n", strerror(errno));
-//        }
-
-//        QCoreApplication a(argc, argv);
-//        MainJob job;
-//        job.working();
-//        return a.exec();
-//    }
-
-//    return 0;
-
-
     QCoreApplication a(argc, argv);
     MainJob job;
     job.working();
