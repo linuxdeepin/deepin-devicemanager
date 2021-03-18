315d583291f2c91eb8a543dd80d402c3d2988fe9
zhangkai <zhangkai@uniontech.com>
2021-03-18 14:11:07 +0800

    chore: 设备管理器前端ut更新
    设备管理器前端ut更新
    
    Log: 设备管理器前端ut更新
    Change-Id: I6033a1809c6a9c8811b54912d9575fe34e74fa7a

---
+++ deepin-devicemanager/tests/DeviceManager/ut_enablemanager.cpp
@@ -0,0 +1,111 @@
+/*
+* Copyright (C) 2019 ~ 2020 UnionTech Software Technology Co.,Ltd
+*
+* Author:      zhangkai <zhangkai@uniontech.com>
+* Maintainer:  zhangkai <zhangkai@uniontech.com>
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "src/DeviceManager/EnableManager.h"
+#include "src/ZeroMQ/ZmqOrder.h"
+#include "../tests/ut_Head.h"
+#include <QCoreApplication>
+#include <QPaintEvent>
+#include <QPainter>
+#include <QProcess>
+#include <QIODevice>
+
+#include <gtest/gtest.h>
+#include <tests/stub.h>
+
+class EnableManager_UT : public UT_HEAD
+{
+public:
+    virtual void setup()
+    {
+    }
+    void TearDown()
+    {
+    }
+};
+
+void ut_start()
+{
+    return;
+}
+
+QByteArray ut_readAllStandardOutput()
+{
+    return "Device Enabled:1";
+}
+
+TEST_F(EnableManager_UT, EnableManager_UT_isDeviceEnable)
+{
+    Stub stub;
+    stub.set((void (QProcess::*)(const QString &, QIODevice::OpenMode))ADDR(QProcess, start), ut_start);
+    stub.set(ADDR(QProcess, readAllStandardOutput), ut_readAllStandardOutput);
+    ASSERT_TRUE(EnableManager::instance()->isDeviceEnable("/"));
+}
+
+TEST_F(EnableManager_UT, EnableManager_UT_isDeviceEnable2)
+{
+    Stub stub;
+    stub.set((void (QProcess::*)(const QString &, QIODevice::OpenMode))ADDR(QProcess, start), ut_start);
+    stub.set(ADDR(QProcess, readAllStandardOutput), ut_readAllStandardOutput);
+    ASSERT_TRUE(EnableManager::instance()->isDeviceEnable(1));
+}
+
+bool ut_execDriverOrder()
+{
+    return false;
+}
+
+TEST_F(EnableManager_UT, EnableManager_UT_enableDeviceByDriver)
+{
+    Stub stub;
+    stub.set(ADDR(ZmqOrder, execDriverOrder), ut_execDriverOrder);
+    ASSERT_EQ(EnableManager::instance()->enableDeviceByDriver(false, "/"), 1);
+}
+
+QByteArray ut_readAllStandardOutput_1()
+{
+    return "";
+}
+
+TEST_F(EnableManager_UT, EnableManager_UT_enablePrinter)
+{
+    Stub stub;
+    stub.set((void (QProcess::*)(const QString &, QIODevice::OpenMode))ADDR(QProcess, start), ut_start);
+    stub.set(ADDR(QProcess, readAllStandardOutput), ut_readAllStandardOutput_1);
+    ASSERT_EQ(EnableManager::instance()->enablePrinter("/", false), 2);
+}
+
+bool ut_execIfconfigOrder()
+{
+    return true;
+}
+
+TEST_F(EnableManager_UT, EnableManager_UT_enableNetworkByIfconfig)
+{
+    Stub stub;
+    stub.set(ADDR(ZmqOrder, execIfconfigOrder), ut_execIfconfigOrder);
+    stub.set((void (QProcess::*)(const QString &, QIODevice::OpenMode))ADDR(QProcess, start), ut_start);
+    stub.set(ADDR(QProcess, readAllStandardOutput), ut_readAllStandardOutput_1);
+    ASSERT_EQ(EnableManager::instance()->enableNetworkByIfconfig("/", false), 2);
+}
+
+TEST_F(EnableManager_UT, EnableManager_UT_getDriverPath)
+{
+    Stub stub;
+    stub.set((void (QProcess::*)(const QString &, QIODevice::OpenMode))ADDR(QProcess, start), ut_start);
+    stub.set(ADDR(QProcess, readAllStandardOutput), ut_readAllStandardOutput_1);
+    EnableManager::instance()->getDriverPath("/");
+}
