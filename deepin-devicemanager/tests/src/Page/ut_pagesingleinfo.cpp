b906cc83709670c6e83a51dbff43a9d16f0ab108
UT000650 <liujuna@uniontech.com>
2021-09-10 15:28:12 +0800

    fix: 修复UT
     优化UT
    
    Log: 修复UT
    Change-Id: I17de952ce6f437dd74b7268b829f239cdc4cf93b

--- deepin-devicemanager/tests/src/Page/ut_pagesingleinfo.cpp
+++ deepin-devicemanager/tests/src/Page/ut_pagesingleinfo.cpp
@@ -14,19 +14,26 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include "../src/Page/PageInfo.h"
-#include "../src/Page/PageSingleInfo.h"
-#include "../src/DeviceManager/DeviceInput.h"
-#include "../src/DeviceManager/DeviceInfo.h"
-#include "../ut_Head.h"
+#include "PageInfo.h"
+#include "PageSingleInfo.h"
+#include "PageTableWidget.h"
+#include "DeviceInput.h"
+#include "DeviceInfo.h"
+#include "ut_Head.h"
+#include "stub.h"
+
+#include <DApplication>
+
 #include <QCoreApplication>
 #include <QPaintEvent>
 #include <QPainter>
+#include <QSignalSpy>
+#include <QClipboard>
+#include <QMenu>
 
 #include <gtest/gtest.h>
-#include "../stub.h"
 
-class PageSingleInfo_UT : public UT_HEAD
+class UT_PageSingleInfo : public UT_HEAD
 {
 public:
     void SetUp()
@@ -40,37 +47,49 @@ public:
     PageSingleInfo *m_PageSingleInfo = nullptr;
 };
 
-TEST_F(PageSingleInfo_UT, PageSingleInfo_UT_updateInfo)
+TEST_F(UT_PageSingleInfo, UT_PageSingleInfo_updateInfo)
 {
     DeviceInput *device = new DeviceInput;
-    QMap<QString, QString> mapinfo;
-    mapinfo.insert("/", "/");
-    device->setInfoFromHwinfo(mapinfo);
+    device->m_Name = "name";
+    device->m_Vendor = "vendor";
+    device->m_Model = "model";
     QList<DeviceBaseInfo *> bInfo;
     bInfo.append(device);
     m_PageSingleInfo->updateInfo(bInfo);
+    DeviceInput *d = dynamic_cast<DeviceInput *>(m_PageSingleInfo->mp_Device);
+    EXPECT_STREQ(d->m_Name.toStdString().c_str(),"name");
+    EXPECT_STREQ(d->m_Vendor.toStdString().c_str(),"vendor");
+    EXPECT_STREQ(d->m_Model.toStdString().c_str(),"model");
     delete device;
 }
 
-TEST_F(PageSingleInfo_UT, PageSingleInfo_UT_setLabel)
+TEST_F(UT_PageSingleInfo, UT_PageSingleInfo_setLabel)
 {
-    m_PageSingleInfo->setLabel("/");
+    m_PageSingleInfo->setLabel("test");
+    EXPECT_STREQ("test",m_PageSingleInfo->mp_Label->text().toStdString().c_str());
     m_PageSingleInfo->clearWidgets();
 }
 
-TEST_F(PageSingleInfo_UT, PageSingleInfo_UT_slotActionRefresh)
+TEST_F(UT_PageSingleInfo, UT_PageSingleInfo_slotActionRefresh)
 {
+    QString conStr=m_PageSingleInfo->mp_Content->toString();
     m_PageSingleInfo->slotActionCopy();
+    QClipboard *clipboard = DApplication::clipboard();
+    QString clipStr = clipboard->text();
+    EXPECT_STREQ(conStr.toStdString().c_str(),clipStr.toStdString().c_str());
+
     m_PageSingleInfo->slotShowMenu(QPoint(0, 0));
+    EXPECT_EQ(m_PageSingleInfo->mp_Menu->actions().size(),1);
 }
 
-TEST_F(PageSingleInfo_UT, PageSingleInfo_UT_slotActionEnable)
+TEST_F(UT_PageSingleInfo, UT_PageSingleInfo_slotActionEnable)
 {
     DeviceInput *device = new DeviceInput;
-    QMap<QString, QString> mapinfo;
-    mapinfo.insert("/", "/");
-    device->setInfoFromHwinfo(mapinfo);
+    device->m_Name = "name";
+    device->m_Vendor = "vendor";
+    device->m_Model = "model";
     m_PageSingleInfo->mp_Device = device;
     m_PageSingleInfo->slotActionEnable();
+    EXPECT_TRUE(m_PageSingleInfo->mp_Device->enable());
     delete device;
 }
