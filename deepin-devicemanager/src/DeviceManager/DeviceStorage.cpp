76f545f7ad8bf799cc8f04c19b47757960f65c0b
UT000650 <liujuna@uniontech.com>
2022-02-08 10:28:08 +0800

    fix: Bug-112677 设备管理器硬盘型号有误_trsid003050
     合并1040分之的修改到1050分之，https://gerrit.uniontech.com/c/deepin-devicemanager/+/110640
     将获取厂商型号的优先级修改为smartctl,当厂商有在固件中提供时，硬盘型号从smartctl中获取更加合理
    
    Log: 【桌面专业版】【神州数码】【龙芯3A5000】【L351】【1043】设备管理器硬盘型号有误_trsid003050
    
    Bug: https://pms.uniontech.com/zentao/bug-view-112677.html
    Change-Id: I9dc41fa1a733d07a73d464309e6d290f32b02730

--- deepin-devicemanager/src/DeviceManager/DeviceStorage.cpp
+++ deepin-devicemanager/src/DeviceManager/DeviceStorage.cpp
@@ -411,28 +411,14 @@ void DeviceStorage::getInfoFromsmartctl(const QMap<QString, QString> &mapInfo)
     // 修正数值
     m_Size.replace(QRegExp(".0[1-9]"), ".00");
 
-    // 型号
-    if (m_Model.isEmpty() || m_Model == "Disk") {
-        //SATA
-        if (mapInfo.find("Device Model") != mapInfo.end())
-            m_Model = mapInfo["Device Model"];
-        //NVME
-        if (mapInfo.find("Model Number") != mapInfo.end())
-            m_Model = mapInfo["Model Number"];
-    }
-
-    //kimtigo 特殊处理，以smartctl数据为准
-    if (mapInfo["Model Number"].indexOf("kimtigo") > -1) {
+    // 通过不断适配，当厂商有在固件中提供时，硬盘型号从smartctl中获取更加合理
+    // 因为hwinfo获取的是主控的型号，而硬盘厂商由于还不能自己生产主控，只能采购别人的主控
+    //SATA
+    if (mapInfo.find("Device Model") != mapInfo.end())
+        m_Model = mapInfo["Device Model"];
+    //NVME
+    if (mapInfo.find("Model Number") != mapInfo.end())
         m_Model = mapInfo["Model Number"];
-    }
-
-    // tf5000 hwinfo --disk获取的硬盘型号为TOSHIBA DT01ACA1 实际为smartctl 中的TOSHIBA DT01ACA100
-    // 在此特殊处理针对该型号硬盘,型号信息从smartctl中获取
-    if (m_Model.startsWith("TOSHIBA DT01ACA1", Qt::CaseInsensitive)) {
-        //SATA
-        if (mapInfo.find("Device Model") != mapInfo.end())
-            m_Model = mapInfo["Device Model"];
-    }
 
     setAttribute(mapInfo, "Serial Number", m_SerialNumber, true);
 }
