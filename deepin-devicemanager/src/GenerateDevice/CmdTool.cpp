f104393cba826f8e862fa3ab789a95c2de963219
liujuna <liujuna@uniontech.com>
2022-03-23 20:21:15 +0800

    test usb audio

--- deepin-devicemanager/src/GenerateDevice/CmdTool.cpp
+++ deepin-devicemanager/src/GenerateDevice/CmdTool.cpp
@@ -432,7 +432,8 @@ void CmdTool::getMulHwinfoInfo(const QString &info)
             continue;
         QMap<QString, QString> mapInfo;
         getMapInfoFromHwinfo(item, mapInfo);
-        if (mapInfo["Hardware Class"] == "sound") {
+        if (mapInfo["Hardware Class"] == "sound" || mapInfo["Device"].contains("USB Audio")) {
+            // mapInfo["Device"].contains("USB Audio") 是为了处理未识别的USB声卡 Bug-118773
             addMapInfo("hwinfo_sound", mapInfo);
         } else if (mapInfo["Hardware Class"] == "network interface") {
             addMapInfo("hwinfo_network", mapInfo);
