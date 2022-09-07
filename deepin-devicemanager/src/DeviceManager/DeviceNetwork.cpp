58ab01fe2aea2ca70c14c27a19c3e7805d5fcf43
luominjiang <luominjiang@uniontech.com>
2022-09-07 15:14:12 +0800

    fix: 开源合规整改
    开源合规整改
    
    Log: 开源合规整改

--- deepin-devicemanager/src/DeviceManager/DeviceNetwork.cpp
+++ deepin-devicemanager/src/DeviceManager/DeviceNetwork.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 // 项目自身文件
 #include "DeviceNetwork.h"
 #include "DBusEnableInterface.h"
