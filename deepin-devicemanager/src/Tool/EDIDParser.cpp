897e3a4bac21742c0ac082b47a71d675e2681977
add-uos <164976197+add-uos@users.noreply.github.com>
2025-04-28 14:49:32 +0800

    fix: the monitor display info in special machine (#453)
    fix the monitor display info in special machine
    
    pick-from: https://github.com/linuxdeepin/deepin-devicemanager/commit/9a46594d310445688c837026581ab205750c60b8
    
    Log: fix the monitor display info in special machine
    Bug: https://pms.uniontech.com/bug-view-310413.html

--- deepin-devicemanager/src/Tool/EDIDParser.cpp
+++ deepin-devicemanager/src/Tool/EDIDParser.cpp
@@ -221,7 +221,7 @@ void EDIDParser::parseScreenSize()
     }
 
     double inch = sqrt((m_Width / 2.54) * (m_Width / 2.54) + (m_Height / 2.54) * (m_Height / 2.54))/10;
-    m_ScreenSize = QString("%1 %2(%3mm X %4mm)").arg(QString::number(inch, '0', 1)).arg(QObject::tr("inch")).arg(m_Width).arg(m_Height);
+    m_ScreenSize = QString("%1 %2(%3mm×%4mm)").arg(QString::number(inch, '0', 1)).arg(QObject::tr("inch")).arg(m_Width).arg(m_Height);
     qCDebug(appLog) << "Screen size parsed:" << m_ScreenSize << "Width:" << m_Width << "Height:" << m_Height;
 }
 
