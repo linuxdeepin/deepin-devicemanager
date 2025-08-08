09917c5a585321ad517fc9d7e25cc5b4fe33c450
JWWTSL <167396168+JWWTSL@users.noreply.github.com>
2025-08-08 13:22:26 +0800

    fix: update translations (#509)
    log:

---
+++ deepin-devicemanager/translations/desktop/desktop_lo.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="lo">
+<context>
+    <name>desktop</name>
+    <message>
+        <location filename="Desktop Entry]Comment" line="0"/>
+        <source>Device Manager is a handy tool for viewing hardware information and managing the devices.</source>
+        <translation>ຕົວຈັດການອຸປະກອນແມ່ນເຄື່ອງມືທີ່ສະດວກສຳລັບເບິ່ງຂໍ້ມູນຮາດແວແລະຈັດການອຸປະກອນ.</translation>
+    </message>
+    <message>
+        <location filename="Desktop Entry]GenericName" line="0"/>
+        <source>Device Manager</source>
+        <translation>ຕົວຈັດການອຸປະກອນ</translation>
+    </message>
+    <message>
+        <location filename="Desktop Entry]Name" line="0"/>
+        <source>Deepin Device Manager</source>
+        <translation>ຕົວຈັດການອຸປະກອນ Deepin</translation>
+    </message>
+</context>
+</TS>
---
+++ deepin-devicemanager/translations/policy/policy_lo.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="lo">
+<context>
+    <name>policy</name>
+    <message>
+        <source>Authentication is required to read hardware information</source>
+        <translation>ຕ້ອງການການຢືນຢັນເພື່ອອ່ານຂໍ້ມູນຮາດແວ</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>ຍົກເລີກ</translation>
+    </message>
+    <message>
+        <source>Confirm</source>
+        <translation>ຢືນຢັນ</translation>
+    </message>
+</context>
+</TS>
